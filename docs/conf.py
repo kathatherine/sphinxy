78bd376b9d927d6876d8cfb15dc17a0d6bc62f9b
Katherine Kinnaman <kkinnaman@anaconda.com>
2023-04-20 15:55:00 -0600

    Second try on sphinx_multiversion

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.githubpages",
-    "sphinx-multiversion",
+    "sphinx_multiversion",
 ]
 
 templates_path = ['_templates']
